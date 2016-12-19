5a42954885c5f7192bef09739aaeb9cd40197756
Jorg Are <jorg2all@yahoo.com>
2016-12-20 00:55:49 +0100

    test(test): write test for the string methods write test for string manipulation methods and make sure all test fails

---
+++ test/string-test.js
@@ -0,0 +1,134 @@
+/* eslint no-unused-expressions: 0 */
+const chai = require('chai');
+const expect = require('chai').expect;
+const deepEqual = chai.deepEqual;
+
+describe('String class', () => {
+
+  describe('hasVowel', () => {
+    it('should return true if a vowel is present in the string', () => {
+      expect('boy'.hasVowels).to.be.true;
+    });
+
+    it('should return false if a vowel is not present in the string', () => {
+      expect('fly'.hasVowels).to.be.false;
+    });
+  });
+
+  describe('toUpper', () => {
+    it('should return uppercase for all characters in a string', () => {
+      expect('boy'.toUpper()).to.equal('BOY');
+    });
+
+    it('should return uppercase for all characters in a string', () => {
+      expect('Goodison PARK'.toUpper()).to.equal('GOODISON PARK');
+    });
+  });
+
+  describe('toLower', () => {
+    it('should return lowercase for all characters in a string', () => {
+      expect('BOY'.toLower()).to.equal('boy');
+    });
+
+    it('should return lowercase for all characters in a string', () => {
+      expect('Goodison PARK'.toLower()).to.equal('goodison park');
+    });
+  });
+
+  describe('ucFirst', () => {
+    it('should return uppercase for first letter in a string', () => {
+      expect('boy'.toLower()).to.equal('Boy');
+    });
+  });
+
+  describe('isQuestion', () => {
+    it('should return true for a valid question', () => {
+      expect('Am I a boy?'.isQuestion()).to.be.true;
+    });
+
+    it('should return false for an invalid question', () => {
+      expect('Is there wood? in the forest'.isQuestion()).to.be.false;
+    });
+  });
+
+  describe('words', () => {
+    it('should return words present in a string', () => {
+      expect('Am I a boy?'.words()).to.deep.equal(['Am', 'I', 'a', 'boy']);
+      expect('Is there woods? in the forest'.words()).eql(['Is', 'there', 'woods', 'in', 'the', 'forest' ]);
+      expect('Life is good'.words()).to.be.an('array');
+    });
+  });
+
+  describe('wordCount', () => {
+    it('should return word count in a string', () => {
+      expect('Am I a boy?'.wordCount()).equal(4);
+      expect('Is there woods? in the forest'.wordCount()).eql(6);
+    });
+  });
+
+  describe('toCurrency', () => {
+    it('should return a number in a currency format', () => {
+      expect('11111'.toCurrency()).equal('11,111');
+      expect('123456789'.toCurrency()).equal('123,456,789');
+    });
+  });
+
+  describe('fromCurrency', () => {
+    it('should return a number in a currency format', () => {
+      expect('11,111'.fromCurrency()).equal(11111);
+      expect('123,456,789'.fromCurrency()).equal(123456789);
+    });
+  });
+
+  describe('inverseCase', () => {
+    it('should returns each letter in the string as an inverse of its current case', () => {
+      expect('Mr. Ben'.inverseCase()).equal('mR. bEN');
+      expect('I am Invisible 10 times'.inverseCase()).equal('i AM iNVISIBLE 10 TIMES');
+    });
+  });
+
+  describe('alternateCase', () => {
+    it('should return the letters in alternating cases', () => {
+      expect('Mr. Ben'.alternateCase()).equal('mR. bEn');
+      expect('Onomatopoeia'.alternateCase()).equal('oNoMaToPoEiA');
+    });
+  });
+
+  describe('getMiddle', () => {
+    it('should return the character in the middle of the string', () => {
+      expect('reads'.getMiddle()).equal('a');
+    });
+
+    it('should return the characters in the middle of the string', () => {
+      expect('read'.getMiddle()).equal('ea');
+    });
+  });
+
+  describe('numberWords', () => {
+    it('should return the numbers in words', () => {
+      expect('325'.numberWords()).equal('three two five');
+      expect('98'.numberWords()).equal('nine eight');
+    });
+  });
+
+  describe('isDigit', () => {
+    it('should return true if the string is a digit', () => {
+      expect('3'.isDigit()).equal(true);
+    });
+
+    it('should return false if the string is not a digit', () => {
+      expect('98'.isDigit()).equal(false);
+    });
+  });
+
+  describe('doubleCheck', () => {
+    it('should return true if a string contains double characters(including whitespace character)', () => {
+      expect('aa'.doubleCheck()).equal(true);
+      expect('!!'.doubleCheck()).equal(true);
+    });
+
+    it('should return false if a string does not contain double character', () => {
+      expect('abcde'.doubleCheck()).equal(false);
+    });
+  });
+});
