7f46a4546cbb7373782b50ec8b8de553bf44553c
Jorg Are <jorg2all@yahoo.com>
2017-01-08 19:35:33 +0100

    feature(front-end): add about to front end

--- public/js/app.js
+++ public/js/app.js
@@ -26,3 +26,24 @@ angular.module('stringClass', []).
       $scope.result = input[method]();
     }
   }]);
+// Get the modal
+const modal = document.getElementById('myModal');
+// Get the button that opens the modal
+const about = document.getElementById('about');
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName('close')[0];
+// When the user clicks the button, open the modal 
+about.onclick = (event) => {
+  event.preventDefault();
+  modal.style.display = 'block';
+}
+// When the user clicks on <span> (x), close the modal
+span.onclick = () => {
+  modal.style.display = 'none';
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = (event) => {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+}
